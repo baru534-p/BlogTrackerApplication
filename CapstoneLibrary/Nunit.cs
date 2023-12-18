a341ba636384e06d3b9cc0bd2e043d2b91f2e6b8
TavvaBhargavi <baru.tavva@gmail.com>
2023-12-18 21:58:21 +0530

    Add project files.

---
+++ CapstoneLibrary/Nunit.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using DataAccessLayer;
+using NUnit.Framework.Legacy;
+using System;
+
+namespace DALTestLayer
+{
+    [TestFixture]
+    public class TestingUsingNUnit
+    {
+
+        [Test]
+        public void checkEmployeeExistWithNunit()
+        {
+            var obj = new EmpInfoRepository();
+            var res = obj.GetEmpInfo("7");
+            Assert.That(res, Is.Not.Null);
+        }
+        [Test]
+        public void checkEmployeeCreateWithNunit()
+        {
+            EmpInfo emp = new EmpInfo
+            {
+                Name = "ABC",
+                EmailId = "As@gmail.com",
+                PassCode = 234231,
+                DateOfJoining = DateTime.Now,
+            };
+            var obj = new BlogDbContext();
+            EmpInfo res = obj.EmpInfo.Add(emp);
+            obj.SaveChanges();
+            ClassicAssert.AreEqual(emp.Name, res.Name);
+            ClassicAssert.AreEqual(emp.EmailId, res.EmailId);
+        }
+    }
+}
